7e12eb8d0ea2051ffb7e177edf979318091848f4
CryDeTaan <crydetaan@gmail.com>
2017-03-23 18:42:44 +0200

    Added Aliases to the aliases.zsh file

--- dotfiles.d/zshrc.d/aliases.zsh
+++ dotfiles.d/zshrc.d/aliases.zsh
@@ -3,5 +3,11 @@
 
 # Simple aliases
 
-# Do the last command as sudo
 alias doit='sudo $(fc -ln -1)'
+alias zshconf="vim ~/.zshrc"
+alias ohmyzsh="vim ~/.oh-my-zsh"
+alias x=startx
+alias i3conf="vim ~/.config/i3/config"
+alias i3blocks="vim ~/.config/i3blocks.conf"
+alias roficonf="vim ~/.config/rofi/config"
+alias termconf="vim .Xresources"
